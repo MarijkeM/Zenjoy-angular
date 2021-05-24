c0128a50e6e8864ef5cd57a38dc1a41130d0c552
Marijke <marijkemeersman@gmail.com>
2021-05-24 18:22:52 +0200

    repaired favorites tabs

--- starbucks-angular/src/app/app.component.ts
+++ starbucks-angular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{  
+  active = 1;
+  ngAfterViewInit(): void {
+
+  }
+  
   title = 'starbucks-angular';
+
+  openReward(evt:any, points:any) {
+    console.log(points);
+
+  };
 }
